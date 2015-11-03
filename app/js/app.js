98999cead940d7933a0605b661267b54e2270b17
Carlos Cessa <carlos@pixative.com>
2015-11-03 14:44:25 -0600

    Login State
    - Defined application `login` state
    - Included sessions module and router

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,17 @@
 'use strict';
 
-define( function () {
-    var app     = angular.module( 'cursame-front', []);
+define( function ( require ) {
+
+    require( 'sessions/SessionsModule' );
+
+    var app     = angular.module( 'cursame-front', [
+            'ui.router',
+            'SessionsModule'
+        ]);
+
+    app.run([ '$rootScope', '$state', function ( $rootScope, $state ) {
+        $state.go( 'login' );
+    }]);
 
     return app;
 });
