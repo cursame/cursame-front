545f10f81ba7bb75bf6a15c9fb21f7684f688794
Carlos Cessa <carlos@pixative.com>
2015-11-29 19:31:33 -0600

    Session End Mock
    - Included mock for the sessions DELETE requests

--- app/js/mocks/MocksConfig.js
+++ app/js/mocks/MocksConfig.js
@@ -1,10 +1,12 @@
 'use strict';
 
 define( function ( require ) {
+    var MocksSessionEnd     = require( 'mocks/MocksSessionEnd' );
     var MocksSessionStart   = require( 'mocks/MocksSessionStart' );
 
     return function ( $httpBackend ) {
         $httpBackend.whenPOST( '/sessions' ).respond( MocksSessionStart );
+        $httpBackend.whenDELETE( '/sessions' ).respond( MocksSessionEnd );
 
         $httpBackend.whenGET( /partials/ ).passThrough();
         $httpBackend.whenGET( /resources/ ).passThrough();
---
+++ app/js/mocks/MocksSessionEnd.js
@@ -0,0 +1,7 @@
+'use strict';
+
+define( function () {
+    return function ( method, url, params ) {
+        return [ 200, {} ];
+    };
+});
