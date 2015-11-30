ee8e58762960e6c2cbbe9a46fd7dae5df281a94b
Carlos Cessa <carlos@pixative.com>
2015-11-29 21:33:47 -0600

    Users Service
    - Created service to interact with the `users` backend resource
    - Included events for the added service

--- app/js/events.js
+++ app/js/events.js
@@ -8,6 +8,12 @@ define( function () {
             'LOGIN_ERROR'       : 'login.error',
             'LOGOUT_SUCCESS'    : 'logout.success',
             'LOGOUT_ERROR'      : 'logout.error',
-            'SYSTEM_MESSAGE'    : 'system_message'
+            'SYSTEM_MESSAGE'    : 'system_message',
+            'USERS_CREATE'      : 'users.create',
+            'USERS_DELETE'      : 'users.delete',
+            'USERS_ERROR'       : 'users.error',
+            'USERS_RETRIEVED'   : 'users.retrieved',
+            'USERS_UPDATED'     : 'users.updated',
+            'USERS_QUERY'       : 'users.query'
         });
 });
--- app/js/users/UsersModule.js
+++ app/js/users/UsersModule.js
@@ -2,8 +2,11 @@
 
 define( function ( require ) {
     var UsersRouter     = require( 'users/UsersRouter' );
+    var UsersService    = require( 'users/UsersService' );
 
     var UsersModule     = angular.module( 'UsersModule', [] );
 
     UsersModule.config([ '$stateProvider', UsersRouter ]);
+
+    UsersModule.factory( 'UsersService', [ 'BaseService', UsersService ] );
 });
