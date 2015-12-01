5b826d76274ab1f1511c94a7c3e6b48faaf724e1
Carlos Cessa <carlos@pixative.com>
2015-12-01 13:20:31 -0600

    User Mock
    - Updated users GET mock to support retrieving the user with the
    user's url parameter as well as the id
    - Included user cover image to the mocked user object

--- app/js/mocks/MocksConfig.js
+++ app/js/mocks/MocksConfig.js
@@ -6,12 +6,13 @@ define( function ( require ) {
     var MocksUsersGet       = require( 'mocks/MocksUsersGet' );
 
     return function ( $httpBackend ) {
+        $httpBackend.whenGET( /partials/ ).passThrough();
+        $httpBackend.whenGET( /resources/ ).passThrough();
+
         $httpBackend.whenPOST( '/sessions' ).respond( MocksSessionStart );
         $httpBackend.whenDELETE( new RegExp( '\\/sessions\\/[0-9]+' ) ).respond( MocksSessionEnd );
 
         $httpBackend.whenGET( new RegExp( '\\/users\\/[0-9]+' ) ).respond( MocksUsersGet );
-
-        $httpBackend.whenGET( /partials/ ).passThrough();
-        $httpBackend.whenGET( /resources/ ).passThrough();
+        $httpBackend.whenGET( new RegExp( '\\/users\\/[a-z]+' ) ).respond( MocksUsersGet );
     };
 });
--- app/js/mocks/MocksUsersGet.js
+++ app/js/mocks/MocksUsersGet.js
@@ -3,23 +3,38 @@
 define( function () {
     return function ( method, url, params ) {
         var paths   = url.split( '/' ),
-            id      = paths[2],
+            param   = paths[2],
             user    = {},
-            error   = false;
+            users   = [{
+                avatar     : 'https://s3.amazonaws.com/uifaces/faces/twitter/BillSKenney/48.jpg',
+                biography  : '',
+                cover      : 'https://cursame.s3.amazonaws.com/uploads/user/coverphoto/56170.png/234985.jpg',
+                email      : 'admin@cursa.me',
+                id         : 1,
+                last_name  : 'Doe',
+                name       : 'John',
+                twitter    : '',
+                url        : 'jdoe'
+            }],
+            error   = false,
+            id      = parseInt( param );
 
-        switch ( id ) {
-            case '1' :
-                user.avatar     = 'https://s3.amazonaws.com/uifaces/faces/twitter/BillSKenney/48.jpg',
-                user.biography  = '';
-                user.email      = 'admin@cursa.me',
-                user.id         = 1;
-                user.last_name  = 'Doe';
-                user.name       = 'John';
-                user.twitter    = '';
-                user.url        = 'jdoe';
-                break;
-            default :
-                error       = true;
+        if ( !isNaN( id ) ) {
+            switch ( id ) {
+                case 1 :
+                    user        = users[0];
+                    break;
+                default :
+                    error       = true;
+            }
+        } else {
+            switch ( param ) {
+                case 'jdoe' :
+                    user        = users[0];
+                    break;
+                default :
+                    error       = true;
+            }
         }
 
         return ( error ) ? [ 403 ] : [ 200, user ];
