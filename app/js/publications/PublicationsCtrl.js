ae7e5bfc24c7a2ee048c51f3ebc1ff9175cc944a
Carlos Cessa <carlos@pixative.com>
2015-12-12 17:33:56 -0500

    Publications Creation
    - Added support to create a comment publication from the logged user
    in the application home page

--- app/js/publications/PublicationsCtrl.js
+++ app/js/publications/PublicationsCtrl.js
@@ -1,10 +1,13 @@
 'use strict';
 
 define( function () {
-    return function ( $scope, Publications ) {
+    return function ( $scope, events, Publications ) {
+        var type    = 'comment';
+
         $scope.panel            = function ( e, id ) {
             var item    = $( e.currentTarget );
 
+            type        = id;
             $('[class*="header active-"]').removeClass(function( i, c ) {
                 return c.match( /active-\d+/g ).join( " " );
             });
@@ -12,6 +15,33 @@ define( function () {
             $( '.panel.active' ).removeClass( 'active' );
             $( '#panel-' + id ).addClass( 'active' );
         };
+        $scope.publish          = function () {
+            var publication     = {
+                access  : 'public',
+                date    : new Date(),
+                user    : $scope.me
+            };
+
+            switch ( type ) {
+                case 'comment' :
+                    publication.content = $scope.comment;
+                    publication.type    = type;
+                    break;
+            }
+
+            Publications.create( publication );
+        };
+
         $scope.publications     = Publications.query();
+
+        $scope.$on( Publications.getEvent( 'CREATE' ), function ( $e, publication ) {
+            $scope.$emit( events.SYSTEM_MESSAGE, {
+                msg     : 'alerts.success.publication_created',
+                type    : 'success'
+            });
+            $scope.comment  = '';
+
+            $scope.publications.unshift( publication );
+        });
     };
 });
