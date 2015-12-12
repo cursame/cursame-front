bbc905ad8ecdfe5527388946c7f87b8a96bed5fc
Carlos Cessa <carlos@pixative.com>
2015-12-12 16:57:50 -0500

    User's Publication
    - Added support to publish comments from the user's profile

--- app/js/users/UsersPublicationsCtrl.js
+++ app/js/users/UsersPublicationsCtrl.js
@@ -2,8 +2,27 @@
 
 define( function () {
     return function ( $scope, $stateParams, events, Publications ) {
-        $scope.publications     = Publications.query({
+        $scope.publications = Publications.query({
             user    : $stateParams.profile
+        });
+
+        $scope.publish      = function () {
+            Publications.create({
+                access  : 'public',
+                content : $scope.comment,
+                date    : new Date(),
+                type    : 'comment',
+                user    : $scope.me
+            });
+        };
+        $scope.$on( Publications.getEvent( 'CREATE' ), function ( $e, publication ) {
+            $scope.$emit( events.SYSTEM_MESSAGE, {
+                msg     : 'alerts.success.publication_created',
+                type    : 'success'
+            });
+            $scope.comment  = '';
+
+            $scope.publications.unshift( publication );
         });
     };
 });
