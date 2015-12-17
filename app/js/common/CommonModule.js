ff9080a89824ca420816c14b9345d513a5602624
Carlos Cessa <carlos@pixative.com>
2015-12-17 01:07:28 -0500

    Select Directive
    - Created directive to define HTML select input with custom styles

--- app/js/common/CommonModule.js
+++ app/js/common/CommonModule.js
@@ -4,10 +4,13 @@ define( function ( require ) {
     var AlertDirective      = require( 'common/AlertDirective' );
     var BaseService         = require( 'common/BaseService' );
     var LocaleService       = require( 'common/LocaleService' );
+    var SelectDirective     = require( 'common/SelectDirective' );
 
     var CommonModule        = angular.module( 'CommonModule', [] );
 
     CommonModule.directive( 'alert', [ '$rootScope', '$timeout', 'events', AlertDirective ] );
+
+    CommonModule.directive( 'cursameSelect', [ '$compile', '$timeout', SelectDirective ] );
 
     CommonModule.factory( 'BaseService', [ '$rootScope', '$resource', 'config', 'events', BaseService ] );
 
---
+++ app/js/common/SelectDirective.js
@@ -0,0 +1,36 @@
+'use strict';
+
+define( function () {
+    return function ( $compile, $timeout ) {
+        return {
+            restrict    : 'E',
+            scope       : {
+                value   : '='
+            },
+            templateUrl : 'partials/common/select.html',
+            transclude  : true,
+            link        : function ( scope, el ) {
+                scope.select    = function ( $e ) {
+                    var elm     = $( $e.target );
+                    $( '.dropdown-menu .active' ).removeClass( 'active' );
+                    elm.addClass( 'active' );
+                    $( 'span.value' ).html( elm.html() );
+                    scope.value     = elm.data( 'value' );
+                };
+
+                $( 'option' ).each( function ( i ) {
+                    var item    = $( this ),
+                        elm     = '<li data-value="' + item.attr( 'value' ) + '" ng-click="select( $event )">' + item.html() + '</li>';
+
+                    item.replaceWith( $compile( elm )( scope ) );
+                });
+
+                $timeout( function () {
+                    scope.select({
+                        target  : $( '.dropdown-menu li', el ).get( 0 )
+                    });
+                }, 0 );
+            }
+        };
+    };
+});
